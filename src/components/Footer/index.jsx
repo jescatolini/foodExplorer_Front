e6904ff94d8a33a899bf8001c9e76b5c51ae650f
Jhonata Escatolini <jescatolini@gmail.com>
2023-06-23 19:25:55 -0300

    feat: add index.jsx Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import DiscreetLogo from '../../assets/discreet-logo.svg';
+import { Wrapper } from '../Wrapper';
+import { Container } from './styles';
+
+export function Footer() {
+  return (
+    <Container>
+      <Wrapper>
+        <div className="brand">
+          <img
+            src={DiscreetLogo}
+            alt="Foto do logotipo"
+          />
+          <h3>food explorer</h3>
+        </div>
+        <p>&copy; 2023 - Todos os direitos reservados</p>
+      </Wrapper>
+    </Container>
+  );
+}
