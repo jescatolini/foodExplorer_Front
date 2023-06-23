3055606d37609d9198d76242e2b22d16d33e704e
Jhonata Escatolini <jescatolini@gmail.com>
2023-06-23 19:07:53 -0300

    feat: add index.jsx Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,26 +1,24 @@
-import styled from 'styled-components';
+import { Container } from './styles';
 
-const Container = styled.button`
-  width: 100%;
-  padding: 15px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  border-radius: 5px;
-  border: ${({ theme, isHighlighted }) =>
-    isHighlighted ? 'none' : `1px solid ${theme.COLORS.WHITE}`};
-  font-size: 1.4rem;
-  color: ${({ theme }) => theme.COLORS.WHITE};
-  background-color: ${({ theme, isHighlighted }) =>
-    isHighlighted ? `${theme.COLORS.RED_900}` : `${theme.COLORS.BLUE_200}`};
-  &:disabled {
-    filter: brightness(0.5);
-    cursor: not-allowed;
-  }
-  svg {
-    font-size: 2.5rem;
-  }
-`;
-
-export { Container };
+export function Button({
+  title,
+  onClick,
+  isHighlighted = true,
+  icon: Icon,
+  type = 'button',
+  disabled = false,
+  ...rest
+}) {
+  return (
+    <Container
+      onClick={onClick}
+      type={type}
+      isHighlighted={isHighlighted}
+      disabled={disabled}
+      {...rest}
+    >
+      {Icon && <Icon />}
+      {title}
+    </Container>
+  );
+}
