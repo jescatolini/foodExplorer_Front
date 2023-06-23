81989f5d9b55bf9f7a54544b330638f47ed259a6
Jhonata Escatolini <jescatolini@gmail.com>
2023-06-23 19:27:14 -0300

    feat: add styles.js Profile page

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  > .my-wrapper {
+    flex-grow: 1;
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 420px;
+  margin: 40px auto;
+  text-align: center;
+  a {
+    font-size: 2.4rem;
+  }
+  h1 {
+    margin: 35px auto 15px;
+    text-align: left;
+    font-size: 3rem;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    line-height: 2.5rem;
+  }
+  .my-input:nth-child(4) {
+    margin-bottom: 45px;
+  }
+`;
+
+export { Container, Form };
