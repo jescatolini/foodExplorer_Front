5be7402e099fd32f406c7eee6074a04a5997369c
Jhonata Escatolini <jescatolini@gmail.com>
2023-06-23 20:25:46 -0300

    feat: add styles Ingredients

---
+++ src/components/Ingredient/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  > img {
+    width: 60px;
+    height: 60px;
+  }
+  p {
+    font-size: clamp(1.2rem, 3vw, 1.8rem);
+  }
+`;
+
+export { Container };
