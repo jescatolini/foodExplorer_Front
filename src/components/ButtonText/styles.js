192743134eb67692ae3bf4d111e6cad0d35d2049
Jhonata Escatolini <jescatolini@gmail.com>
2023-06-23 18:47:29 -0300

    feat: add styles.js ButtonText

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+import { Link } from 'react-router-dom';
+
+const Container = styled(Link)`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  p {
+    font-size: clamp(1.4rem, 4vw, 2.4rem);
+  }
+`;
+
+export { Container };
