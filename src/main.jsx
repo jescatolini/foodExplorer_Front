cab3483aa02de7148fe192118454baa9abd40842
Jhonata Escatolini <jescatolini@gmail.com>
2023-06-26 16:27:27 -0300

    feat: add main to index.html app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+
+import { AuthProvider } from './hooks/auth';
+import { CartProvider } from './hooks/cart';
+import { MealsProvider } from './hooks/meals';
+import { RequestProvider } from './hooks/request';
+import { Routes } from './routes/index';
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RequestProvider>
+        <AuthProvider>
+          <CartProvider>
+            <MealsProvider>
+              <Routes />
+            </MealsProvider>
+          </CartProvider>
+        </AuthProvider>
+      </RequestProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
